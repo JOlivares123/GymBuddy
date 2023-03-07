85817a276615ab5c0b59baf58e98f54d0a0e60be
JOlivares123 <jacobolivares123@gmail.com>
2023-03-06 19:20:27 -0800

    made header use h2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
         <header className="App-header">
             <div className='d-flex flex-row align-items-center'>
                 <img className='d-flex Header-logo' alt='dumbbell logo' src='/logo72.png' />
-                <h1 className='ps-2 Header-font'>Gym Buddy</h1>
+                <h2 className='ps-2 Header-font'>Gym Buddy</h2>
             </div>
         </header>
     )
