7de91fe4e1efa3500282184135dfcb641dd257f3
JOlivares123 <jacobolivares123@gmail.com>
2023-03-07 18:43:14 -0800

    installed react-hook-form + fixed styling for login + signup pages (centered)

--- src/routes/Login/LoginPage.jsx
+++ src/routes/Login/LoginPage.jsx
@@ -1,7 +1,22 @@
+import { CustButton } from '../../components/Button/CustButton'
+import './LoginPage.scss'
+
 export const LoginPage = () => {
     return(
-        <div>
-            hello Login page
-        </div>
+        <>
+            <h1 className="mt-5">Login</h1>
+            <form className='me-auto ms-auto col-7 mt-4 text-start'>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" class="form-control TransparentInput white" id="email" aria-describedby="emailHelp"/>
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control TransparentInput white" id="password"/>
+                </div>
+            </form>
+            <CustButton text='Login' className='px-4 mt-3' />
+            <h5 className="mt-5">Need an account? <a className='white' href="/signup"> Sign Up</a></h5>
+        </>
     )
 }
--- src/routes/Signup/SignupPage.jsx
+++ src/routes/Signup/SignupPage.jsx
@@ -1,7 +1,31 @@
+import { useForm } from "react-hook-form"
+import { CustButton } from "../../components/Button/CustButton"
+import './SignupPage.scss'
+
 export const SignupPage = () => {
     return(
-        <div>
-            hello Signup page
-        </div>
+        <>
+            <h1 className="mt-5">Sign Up</h1>
+            <form className='me-auto ms-auto col-7 mt-4 text-start'>
+                <div class="mb-3">
+                    <label for="firstName" class="form-label">First Name <span style={{color: 'red'}}>*</span></label>
+                    <input type="firstName" class="form-control TransparentInput white" id="firstName" aria-describedby="firstNameHelp"/>
+                </div>
+                <div class="mb-3">
+                    <label for="lastName" class="form-label">Last Name <span style={{color: 'red'}}>*</span></label>
+                    <input type="lastName" class="form-control TransparentInput white" id="lastName" aria-describedby="lastNameHelp"/>
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address <span style={{color: 'red'}}>*</span></label>
+                    <input type="email" class="form-control TransparentInput white" id="email" aria-describedby="emailHelp"/>
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password <span style={{color: 'red'}}>*</span></label>
+                    <input type="password" class="form-control TransparentInput white" id="password"/>
+                </div>
+            </form>
+            <CustButton text='Sign Up' className='px-4 mt-3' />
+            <h5 className="mt-5">Already a user? <a className='white' href="/login"> Login</a></h5>
+        </>
     )
 }
