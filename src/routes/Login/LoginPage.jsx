cbf5174891d1dcec407ce3a5ce08be2d98425afd
JOlivares123 <jacobolivares123@gmail.com>
2023-03-07 18:51:20 -0800

    cleared console errors for wrong html attrbutes

--- src/routes/Login/LoginPage.jsx
+++ src/routes/Login/LoginPage.jsx
@@ -6,13 +6,13 @@ export const LoginPage = () => {
         <>
             <h1 className="mt-5">Login</h1>
             <form className='me-auto ms-auto col-7 mt-4 text-start'>
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email address</label>
-                    <input type="email" class="form-control TransparentInput white" id="email" aria-describedby="emailHelp"/>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control TransparentInput white" id="email" aria-describedby="emailHelp"/>
                 </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control TransparentInput white" id="password"/>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control TransparentInput white" id="password"/>
                 </div>
             </form>
             <CustButton text='Login' className='px-4 mt-3' />
--- src/routes/Signup/SignupPage.jsx
+++ src/routes/Signup/SignupPage.jsx
@@ -7,21 +7,21 @@ export const SignupPage = () => {
         <>
             <h1 className="mt-5">Sign Up</h1>
             <form className='me-auto ms-auto col-7 mt-4 text-start'>
-                <div class="mb-3">
-                    <label for="firstName" class="form-label">First Name <span style={{color: 'red'}}>*</span></label>
-                    <input type="firstName" class="form-control TransparentInput white" id="firstName" aria-describedby="firstNameHelp"/>
+                <div className="mb-3">
+                    <label htmlFor="firstName" className="form-label">First Name <span style={{color: 'red'}}>*</span></label>
+                    <input type="firstName" className="form-control TransparentInput white" id="firstName" aria-describedby="firstNameHelp"/>
                 </div>
-                <div class="mb-3">
-                    <label for="lastName" class="form-label">Last Name <span style={{color: 'red'}}>*</span></label>
-                    <input type="lastName" class="form-control TransparentInput white" id="lastName" aria-describedby="lastNameHelp"/>
+                <div className="mb-3">
+                    <label htmlFor="lastName" className="form-label">Last Name <span style={{color: 'red'}}>*</span></label>
+                    <input type="lastName" className="form-control TransparentInput white" id="lastName" aria-describedby="lastNameHelp"/>
                 </div>
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email address <span style={{color: 'red'}}>*</span></label>
-                    <input type="email" class="form-control TransparentInput white" id="email" aria-describedby="emailHelp"/>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address <span style={{color: 'red'}}>*</span></label>
+                    <input type="email" className="form-control TransparentInput white" id="email" aria-describedby="emailHelp"/>
                 </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Password <span style={{color: 'red'}}>*</span></label>
-                    <input type="password" class="form-control TransparentInput white" id="password"/>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password <span style={{color: 'red'}}>*</span></label>
+                    <input type="password" className="form-control TransparentInput white" id="password"/>
                 </div>
             </form>
             <CustButton text='Sign Up' className='px-4 mt-3' />
