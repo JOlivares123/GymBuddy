157eef5cc68ec6f7a2d3da93a0ba0ec933003c13
JOlivares123 <jacobolivares123@gmail.com>
2023-03-08 19:12:22 -0800

    updated seondary black color + fixed css for app container width

--- src/routes/Home/HomePage.jsx
+++ src/routes/Home/HomePage.jsx
@@ -3,7 +3,7 @@ import './HomePage.scss'
 export const HomePage = ({name='Jacob'}) => {
     return(
         <div>
-            <h1 className='bolderText'>Welcome {name}</h1>
+            <h1 className='text-start pt-5 ps-3'>Welcome {name}</h1>
             <div>
 
             </div>
