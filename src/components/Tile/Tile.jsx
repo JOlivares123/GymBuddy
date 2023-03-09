d5b4d1d41768fc3389a80990f28e4a7359b7c1c5
JOlivares123 <jacobolivares123@gmail.com>
2023-03-09 13:38:01 -0800

    fixed Tile display - homepage styled completely now

--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -3,7 +3,7 @@ import './Tile.scss'
 
 export const Tile = ({title, link, desc, buttonTxt, className, buttonColor}) => {
     return(
-        <div className={`Tile TileAboveMobile TileMobile d-inline-block ${className}`}>
+        <div className={`Tile TileAboveMobile TileMobile d-flex ${className}`}>
             <h3 className={buttonColor}>{title}</h3>
             <p className='pb-1'>{desc}</p>
             <CustButton color={buttonColor} className='px-5' text={buttonTxt} link={link}/>
