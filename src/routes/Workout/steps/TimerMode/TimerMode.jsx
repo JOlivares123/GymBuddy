d569d1bab4d7bb2aef50ab0e2e7d2d111da910c0
JOlivares123 <jacobolivares123@gmail.com>
2023-04-12 18:14:13 -0700

    added styling for TimerMode

--- src/routes/Workout/steps/TimerMode/TimerMode.jsx
+++ src/routes/Workout/steps/TimerMode/TimerMode.jsx
@@ -1,16 +1,16 @@
 import PropTypes from "prop-types";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
-const renderTime = ({ remainingTime }) => {
-  if (remainingTime === 0) {
-    return <div className="timer">Continue Workout!</div>;
-  }
+import "./TimerMode.scss";
 
+const renderTime = ({ remainingTime }) => {
+  const mins = Math.floor((remainingTime % 3600) / 60);
+  const secs = remainingTime % 60;
+
+  // adding extra 0s where needed
   return (
     <div className="timer">
-      <div className="text">Remaining</div>
-      <div className="value">{remainingTime}</div>
-      <div className="text">seconds</div>
+      <h1>{`${mins < 10 && "0"}${mins}:${secs < 10 ? "0" : ""}${secs}`}</h1>
     </div>
   );
 };
@@ -18,16 +18,28 @@ const renderTime = ({ remainingTime }) => {
 export const TimerMode = ({ prev, restTimeSecs }) => {
   // use prev(2) to navigate back to Rest.jsx
   return (
-    <div>
-      <CountdownCircleTimer
-        isPlaying={true}
-        colors="#EBAA28"
-        duration={restTimeSecs}
-        onComplete={() => prev(2)}
-      >
-        {renderTime}
-      </CountdownCircleTimer>
-    </div>
+    <>
+      <h1 className="fw-bold my-5">Resting Time</h1>
+      <div className="timer-wrapper">
+        <CountdownCircleTimer
+          size={300}
+          strokeWidth={16}
+          isPlaying={true}
+          colors={["#EB4828", "#EB286A", "#EBAA28", "#6E9887", "#386641"]}
+          colorsTime={[
+            restTimeSecs / 4,
+            (restTimeSecs / 4) * 3,
+            (restTimeSecs / 4) * 2,
+            restTimeSecs / 4,
+            0,
+          ]}
+          duration={restTimeSecs}
+          onComplete={() => prev(2)}
+        >
+          {renderTime}
+        </CountdownCircleTimer>
+      </div>
+    </>
   );
 };
 
