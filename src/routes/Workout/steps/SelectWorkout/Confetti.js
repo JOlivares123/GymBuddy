dad39b7cd4cc1e19a66e9bc1709d612ecd4cd436
JOlivares123 <jacobolivares123@gmail.com>
2023-04-14 17:22:33 -0700

    updated colors for confetti effect

--- src/routes/Workout/steps/SelectWorkout/Confetti.js
+++ src/routes/Workout/steps/SelectWorkout/Confetti.js
@@ -17,23 +17,28 @@ export const confettiEffect = () => {
   fire(0.25, {
     spread: 26,
     startVelocity: 55,
+    colors: ["#EB4828", "#EBAA28", "#EB286A", "#386641"],
   });
   fire(0.2, {
     spread: 60,
+    colors: ["#EB4828", "#EBAA28", "#EB286A", "#386641"],
   });
   fire(0.35, {
     spread: 100,
     decay: 0.91,
     scalar: 0.8,
+    colors: ["#EB4828", "#EBAA28", "#EB286A", "#386641"],
   });
   fire(0.1, {
     spread: 120,
     startVelocity: 25,
     decay: 0.92,
     scalar: 1.2,
+    colors: ["#EB4828", "#EBAA28", "#EB286A", "#386641"],
   });
   fire(0.1, {
     spread: 120,
     startVelocity: 45,
+    colors: ["#EB4828", "#EBAA28", "#EB286A", "#386641"],
   });
 };
