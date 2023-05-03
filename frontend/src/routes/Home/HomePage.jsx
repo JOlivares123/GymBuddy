3df3f019a4a39ac2d45be029c48ae9b7869cd25c
JOlivares123 <jacobolivares123@gmail.com>
2023-05-03 15:44:29 -0700

    can now reach backend locally

--- frontend/src/routes/Home/HomePage.jsx
+++ frontend/src/routes/Home/HomePage.jsx
@@ -1,10 +1,20 @@
 import PropTypes from "prop-types";
+// import { useEffect } from "react";
+// import axios from "axios";
 
 import { Tile } from "../../components/Tile/Tile";
 import data from "./HomePageCopy.json";
 import "./HomePage.scss";
 
 export const HomePage = ({ name = "Jacob" }) => {
+  // can now fetch backend api endpoints
+  // useEffect(() => {
+  //   const loadData = async () => {
+  //     const resp = await axios.get("http://localhost:3001/api");
+  //     console.log(resp.data);
+  //   };
+  //   loadData();
+  // }, []);
   return (
     <div>
       <h1 className="text-center text-sm-center text-md-start text-lg-start text-xl-start py-5">
