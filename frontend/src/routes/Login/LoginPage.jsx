49c286ce5fd77b245b80afb41aac7c30c6b6b5b0
JOlivares123 <jacobolivares123@gmail.com>
2023-05-08 20:32:03 -0700

    updated frontend protion for login and signup

--- frontend/src/routes/Login/LoginPage.jsx
+++ frontend/src/routes/Login/LoginPage.jsx
@@ -1,20 +1,64 @@
 // import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 import { CustButton } from "../../components/Button/CustButton";
 import { isMobile } from "../../utils/WindowSize";
 import "./LoginPage.scss";
 
 export const LoginPage = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [apiData, setApiData] = useState(null);
+
+  useEffect(() => {
+    if (apiData?.user) {
+      setIsLoading(false);
+      // redirect to homepage
+      navigate("/home");
+    }
+  }, [apiData]);
+
+  const logUserIn = async () => {
+    setError(null);
+
+    try {
+      setIsLoading(true);
+
+      const res = await axios.post(
+        "http://localhost:3001/api/login",
+        JSON.stringify({
+          email: email,
+          password: password,
+        }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      console.log(res);
+      setApiData(res.data);
+    } catch (err) {
+      setError(err);
+    }
+  };
+
   return (
     <>
       <h1 className="mt-5">Login</h1>
       <form className="me-auto ms-auto col-7 mt-4 text-start">
         <div className="mb-3">
           <label htmlFor="email" className="form-label">
-            Email address
+            Email Address
           </label>
           <input
             type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="form-control TransparentInput white"
             id="email"
             aria-describedby="emailHelp"
@@ -25,16 +69,24 @@ export const LoginPage = () => {
             Password
           </label>
           <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             type="password"
             className="form-control TransparentInput white"
             id="password"
           />
         </div>
       </form>
-      <CustButton
-        text="Login"
-        className={isMobile() ? "col-7 mt-3" : "px-4 mt-3"}
-      />
+      {isLoading ? (
+        <div className="fw-bold yellow mt-3">LOADING ...</div>
+      ) : (
+        <CustButton
+          text="Login"
+          onClick={async () => await logUserIn()}
+          className={isMobile() ? "col-7 mt-3" : "px-4 mt-3"}
+        />
+      )}
+      {error && <div className="fw-bold my-3 pink">Error: {error?.data}</div>}
       <h5 className="mt-5">
         Need an account?{" "}
         <a className="white" href="/signup">
--- frontend/src/routes/Signup/SignupPage.jsx
+++ frontend/src/routes/Signup/SignupPage.jsx
@@ -1,10 +1,57 @@
 // import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 import { isMobile } from "./../../utils/WindowSize";
 import { CustButton } from "../../components/Button/CustButton";
 import "./SignupPage.scss";
 
 export const SignupPage = () => {
+  const navigate = useNavigate();
+  const [apiData, setApiData] = useState(null);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // fix how i am calling this. I need to wait for signUserUp to finish before trying
+  // to send the user over
+  const signUserUp = async () => {
+    // reset errors
+    setError(null);
+    try {
+      setIsLoading(true);
+      const res = await axios.post(
+        "http://localhost:3001/api/signup",
+        JSON.stringify({
+          email: email,
+          password: password,
+          firstName: firstName,
+          lastName: lastName,
+        }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+
+      setApiData(res.data);
+    } catch (err) {
+      setError(err.response?.data);
+    }
+  };
+
+  useEffect(() => {
+    if (apiData?.user) {
+      setIsLoading(false);
+      // redirect to homepage
+      navigate("/home");
+    }
+  }, [apiData]);
+
   return (
     <>
       <h1 className="mt-5">Sign Up</h1>
@@ -14,9 +61,12 @@ export const SignupPage = () => {
             First Name <span style={{ color: "red" }}>*</span>
           </label>
           <input
-            type="firstName"
             className="form-control TransparentInput white"
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
             id="firstName"
+            required
             aria-describedby="firstNameHelp"
           />
         </div>
@@ -25,21 +75,28 @@ export const SignupPage = () => {
             Last Name <span style={{ color: "red" }}>*</span>
           </label>
           <input
-            type="lastName"
+            onChange={(e) => {
+              setLastName(e.target.value);
+            }}
             className="form-control TransparentInput white"
             id="lastName"
+            required
             aria-describedby="lastNameHelp"
           />
         </div>
         <div className="mb-3">
           <label htmlFor="email" className="form-label">
-            Email address <span style={{ color: "red" }}>*</span>
+            Email Address <span style={{ color: "red" }}>*</span>
           </label>
           <input
             type="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
             className="form-control TransparentInput white"
             id="email"
             aria-describedby="emailHelp"
+            required
           />
         </div>
         <div className="mb-3">
@@ -47,16 +104,31 @@ export const SignupPage = () => {
             Password <span style={{ color: "red" }}>*</span>
           </label>
           <input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             type="password"
+            required
             className="form-control TransparentInput white"
             id="password"
           />
         </div>
       </form>
-      <CustButton
-        text="Sign Up"
-        className={isMobile() ? "col-7 mt-3" : "px-4 mt-3"}
-      />
+      {isLoading ? (
+        <div className="fw-bold yellow mt-3">LOADING ...</div>
+      ) : (
+        <CustButton
+          text="Sign Up"
+          onClick={async () => await signUserUp()}
+          className={isMobile() ? "col-7 mt-3" : "px-4 mt-3"}
+        />
+      )}
+
+      {error && (
+        <div className="fw-bold my-3 pink">
+          Error: {error?.email || error?.password}
+        </div>
+      )}
       <h5 className="mt-5">
         Already a user?{" "}
         <a className="white" href="/login">
